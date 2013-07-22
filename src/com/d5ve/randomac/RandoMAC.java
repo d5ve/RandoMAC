3c16ee5afdfa239c197654e93cb16c0985a9de1e
Dave Webb <github@d5ve.com>
2013-07-23 00:23:12 +0100

    Major changes towards actually working
    Now generates random MACs, plus has entry and button for "setting".
    
    ***	modified:   AndroidManifest.xml
    
    Removed DisplayMAC activity from manifest.
    
    ***	modified:   res/layout/main.xml
    
    Renamed strings/ids to something more suitable.
    
    ***	modified:   res/values/strings.xml
    
    Added/edited string resources for the above.
    
    ***	deleted:    src/com/d5ve/randomac/DisplayMAC.java
    
    Removed unneeded activity.
    
    ***	modified:   src/com/d5ve/randomac/RandoMAC.java
    
    Added setMac(), getCurrentMacAddress(), and getRandomMacAddress().
    
    Removed sendMessage()

--- src/com/d5ve/randomac/RandoMAC.java
+++ src/com/d5ve/randomac/RandoMAC.java
@@ -1,10 +1,16 @@
 package com.d5ve.randomac;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Random;
 
 public class RandoMAC extends Activity
 {
@@ -16,14 +22,52 @@ public class RandoMAC extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        EditText editText = (EditText) findViewById(R.id.new_mac);
+        editText.setHint(getRandomMacAddress());
+
+        String current_mac = getCurrentMacAddress();
+        TextView currentMac = (TextView) findViewById(R.id.current_mac);
+        currentMac.setTextSize(30);
+        currentMac.setText( current_mac );
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMAC.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    /** Called when the user clicks the Set MAC button */
+    public void setMac(View view) {
+        // Shelling out goes here
+        TextView currentMac = (TextView) findViewById(R.id.current_mac);
+        currentMac.setTextSize(30);
+        EditText editText = (EditText) findViewById(R.id.new_mac);
+        String new_mac = editText.getText().toString();
+        if ( new_mac.equals("") ) {
+            new_mac = editText.getHint().toString();
+        }
+        currentMac.setText( new_mac );
+        editText.setHint( getRandomMacAddress() );
+    }
+
+    public String getCurrentMacAddress() {
+        WifiManager wifiMan = (WifiManager) this.getSystemService( Context.WIFI_SERVICE );
+        WifiInfo wifiInfo = wifiMan.getConnectionInfo();
+        String macAddress = wifiInfo.getMacAddress();
+        return macAddress;
+    }
+
+    // Not totally random - it uses an assigned OUI block
+    public String getRandomMacAddress() {
+
+        String macAddressOui = "28:37:37"; // Apple Computers
+
+        StringBuilder s = new StringBuilder(17);
+        s.append(macAddressOui);
+
+        Random r = new Random();
+        for (int i = 0; i < 3; i++) {
+            s.append(":");
+            int j = r.nextInt(255);
+            s.append(String.format("%02x", j));
+        }
+
+        return s.toString();
     }
 }
